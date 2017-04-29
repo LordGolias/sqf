db5b211861b2da791992c2ed293a1becf5132e23
Lord Golias <lord.golias1@gmail.com>
2017-04-29 05:25:27 +0200

    Moved basic interpret functionality to own class.

---
+++ sqf/base_interpreter.py
@@ -0,0 +1,125 @@
+from sqf.types import Statement, Code, ConstantValue, Number, Nothing, Variable, Array, String, Type
+from sqf.interpreter_types import InterpreterType
+from sqf.keywords import Keyword
+from sqf.exceptions import SQFSyntaxError
+from sqf.namespace import Namespace
+
+
+class BaseInterpreter:
+    """
+    Base Interpreter used by the analyzer and interpreter
+    """
+    def __init__(self, all_vars=None):
+        self._namespaces = {
+            'uiNamespace': Namespace(),
+            'parsingNamespace': Namespace(),
+            'missionNamespace': Namespace(all_vars),
+            'profileNamespace': Namespace()
+        }
+
+        self._current_namespace = self._namespaces['missionNamespace']
+
+    def exception(self, exception):
+        """
+        We can overwrite this method to handle exceptions differently
+        """
+        raise exception
+
+    def set_global_variable(self, var_name, value):
+        assert(isinstance(value, Type))
+        self._namespaces['missionNamespace'].base_scope[var_name] = value
+
+    @property
+    def namespaces(self):
+        return self._namespaces
+
+    @property
+    def current_scope(self):
+        return self._current_namespace.current_scope
+
+    @property
+    def values(self):
+        return self.current_scope
+
+    def __getitem__(self, name):
+        return self.current_scope[name]
+
+    def __contains__(self, other):
+        return other in self.values
+
+    def value(self, token, namespace_name=None):
+        if isinstance(token, Variable):
+            scope = self.get_scope(token.name, namespace_name)
+            return scope[token.name]
+        elif isinstance(token, (ConstantValue, Array, Number, Code, Keyword, InterpreterType)):
+            return token
+        else:
+            raise NotImplementedError(repr(token))
+
+    def get_variable(self, token):
+        if isinstance(token, Statement):
+            return token.base_tokens[0]
+        else:
+            if not isinstance(token, Variable):
+                self.exception(SQFSyntaxError(token.position, 'This must be a variable'))
+            return token
+
+    def execute_token(self, token):
+        """
+        Given a single token, recursively evaluate it and return its value.
+        """
+        raise NotImplementedError
+
+    def get_scope(self, name, namespace_name=None):
+        if namespace_name is None:
+            namespace = self._current_namespace
+        else:
+            namespace = self._namespaces[namespace_name]
+        return namespace.get_scope(name)
+
+    def add_scope(self, values=None):
+        self._current_namespace.add_scope(values)
+
+    def del_scope(self):
+        self._current_namespace.del_scope()
+
+    def add_privates(self, variables):
+        for variable in variables:
+            if isinstance(variable, Variable):
+                name = variable.name
+            else:
+                assert(isinstance(variable, String))
+                name = variable.value
+
+            if not name.startswith('_'):
+                self.exception(SQFSyntaxError(variable.position, 'Cannot use global variable "%s" in `private`' % name))
+            self.current_scope[name] = Nothing
+
+    def execute_code(self, code, params=None, extra_scope=None):
+        assert (isinstance(code, Code))
+        if params is None:
+            params = Array([])
+        if extra_scope is None:
+            extra_scope = {}
+        extra_scope['_this'] = params
+        self.add_scope(extra_scope)
+        outcome = Nothing
+        for statement in code.base_tokens:
+            outcome = self.execute_single(statement)
+        self.del_scope()
+        return outcome
+
+    def execute_single(self, statement):
+        """
+        Executes a statement
+        """
+        raise NotImplementedError
+
+    def execute(self, statements):
+        """
+        Executes a list of statements
+        """
+        outcome = Nothing
+        for statement in statements:
+            outcome = self.execute_single(statement)
+        return outcome
--- sqf/interpreter.py
+++ sqf/interpreter.py
@@ -1,31 +1,27 @@
 from sqf.types import Statement, Code, ConstantValue, Number, Boolean, Nothing, Variable, Array, String, Type
-from sqf.interpreter_types import InterpreterType
 from sqf.keywords import Keyword
 from sqf.object import Marker
 from sqf.parser import parse
 from sqf.exceptions import ExecutionError, SQFSyntaxError, SQFError
 from sqf.expressions import EXPRESSIONS
-from sqf.namespace import Namespace
+from sqf.base_interpreter import BaseInterpreter
 
 
-class Interpreter:
+class Interpreter(BaseInterpreter):
 
     def __init__(self, all_vars=None):
-        # the stack of scopes. The outermost also contains global variables
-        self._namespaces = {
-            'uiNamespace': Namespace(),
-            'parsingNamespace': Namespace(),
-            'missionNamespace': Namespace(all_vars),
-            'profileNamespace': Namespace()
-        }
-
-        self._current_namespace = self._namespaces['missionNamespace']
-        # self._stack = [Scope(all_vars)]
+        super().__init__(all_vars)
 
         self._markers = {}
 
         self._simulation = None
         self._client = None
+
+    @property
+    def simulation(self):
+        if self._simulation is None:
+            raise ExecutionError('Trying to access simulation without a simulation assigned')
+        return self._simulation
 
     @property
     def client(self):
@@ -39,54 +35,8 @@ class Interpreter:
         self._simulation = client.simulation
 
     @property
-    def simulation(self):
-        if self._simulation is None:
-            raise ExecutionError('Trying to access simulation without a simulation assigned')
-        return self._simulation
-
-    def set_global_variable(self, var_name, value):
-        assert(isinstance(value, Type))
-        self._namespaces['missionNamespace'].base_scope[var_name] = value
-
-    @property
-    def namespaces(self):
-        return self._namespaces
-
-    @property
-    def current_scope(self):
-        return self._current_namespace.current_scope
-
-    @property
-    def values(self):
-        return self.current_scope
-
-    def __getitem__(self, name):
-        return self.current_scope[name]
-
-    def __contains__(self, other):
-        return other in self.values
-
-    @property
     def markers(self):
         return self._markers
-
-    def value(self, token, namespace_name=None):
-        if isinstance(token, Variable):
-            scope = self.get_scope(token.name, namespace_name)
-            return scope[token.name]
-        elif isinstance(token, (ConstantValue, Array, Code, Keyword, InterpreterType)):
-            return token
-        else:
-            raise NotImplementedError(repr(token))
-
-    @staticmethod
-    def get_variable(token):
-        if isinstance(token, Statement):
-            return token.base_tokens[0]
-        else:
-            if not isinstance(token, Variable):
-                raise SQFSyntaxError(token.position, 'This must be a variable')
-            return token
 
     def execute_token(self, token):
         """
@@ -108,45 +58,6 @@ class Interpreter:
             result = token
 
         return result, self.value(result)
-
-    def get_scope(self, name, namespace_name=None):
-        if namespace_name is None:
-            namespace = self._current_namespace
-        else:
-            namespace = self._namespaces[namespace_name]
-        return namespace.get_scope(name)
-
-    def add_scope(self, vars=None):
-        self._current_namespace.add_scope(vars)
-
-    def del_scope(self):
-        self._current_namespace.del_scope()
-
-    def add_privates(self, variables):
-        for variable in variables:
-            if isinstance(variable, Variable):
-                name = variable.name
-            else:
-                assert(isinstance(variable, String))
-                name = variable.value
-
-            if not name.startswith('_'):
-                raise SQFSyntaxError(variable.position, 'Cannot use global variable "%s" in `private`' % name)
-            self.current_scope[name] = Nothing
-
-    def execute_code(self, code, params=None, extra_scope=None):
-        assert (isinstance(code, Code))
-        if params is None:
-            params = Array([])
-        if extra_scope is None:
-            extra_scope = {}
-        extra_scope['_this'] = params
-        self.add_scope(extra_scope)
-        outcome = Nothing
-        for statement in code.base_tokens:
-            outcome = self.execute_single(statement)
-        self.del_scope()
-        return outcome
 
     def execute_single(self, statement):
         assert(not isinstance(statement, Code))
@@ -244,12 +155,6 @@ class Interpreter:
 
         if statement.ending:
             outcome = Nothing
-        return outcome
-
-    def execute(self, statements):
-        outcome = Nothing
-        for statement in statements:
-            outcome = self.execute_single(statement)
         return outcome
 
     def create_marker(self, rhs_v):
