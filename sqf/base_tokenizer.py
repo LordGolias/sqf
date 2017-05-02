36de04e2305da9b60cd61ac27b534c9022648ae2
Lord Golias <lord.golias1@gmail.com>
2017-05-02 15:22:53 +0200

    Added missing operators to tokenizer.

--- sqf/base_tokenizer.py
+++ sqf/base_tokenizer.py
@@ -29,7 +29,7 @@ def flatten(l):
 def tokenize(statement):
     # the len=2 tokens have to be first!
     keywords = ('\\\n', '>>', '/*', '*/', '||', '//', '!=', '<=', '>=', '==', '"', "'", ' ', '=', ':', '{', '}',
-                '(', ')', '[', ']', ';', ',', '!', '\n', '\t', '/', '*', '%', '^', '-', '+')
+                '(', ')', '[', ']', ';', ',', '!', '\n', '\t', '/', '*', '%', '^', '-', '+', '<', '>')
 
     # todo: do not use recursion to avoid setting this limit.
     old_value = sys.getrecursionlimit()
