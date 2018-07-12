0f1c9ea35712ac396527bcaac97d0e703bf17133
SilentSpike <SilentSpike100+Github@gmail.com>
2018-07-12 10:54:09 +0100

    Add ## preprocessor command to tokenizer

--- sqf/base_tokenizer.py
+++ sqf/base_tokenizer.py
@@ -3,5 +3,5 @@ import re
 
 def tokenize(statement):
     # the len=2 tokens have to be first!
-    regex = r'(\#include|#else|\#endif|\#ifndef|\#ifdef|\#define|\\\n|\r\n|>>|\/\*|\*\/|\|\||//|!=|<=|>=|==|\n|\t|[\"\' =:\{\}\(\)\[\];/,\!\/\#\*\%\^\-\+<>])'
+    regex = r'(\#\#|\#include|\#else|\#endif|\#ifndef|\#ifdef|\#define|\\\n|\r\n|>>|\/\*|\*\/|\|\||//|!=|<=|>=|==|\n|\t|[\"\' =:\{\}\(\)\[\];/,\!\/\#\*\%\^\-\+<>])'
     return list(filter(None, re.split(regex, statement)))
