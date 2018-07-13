749d0b10b89a33f65aefc519fc87d6eb0010a93c
Lord Golias <lord.golias1@gmail.com>
2018-07-13 12:07:24 +0200

    Removed unused function.

--- sqf/base_type.py
+++ sqf/base_type.py
@@ -1,16 +1,5 @@
 
 
-def equal_dicts(d1, d2, ignore_keys):
-    ignored = set(ignore_keys)
-    for k1, v1 in d1.items():
-        if k1 not in ignored and (k1 not in d2 or d2[k1] != v1):
-            return False
-    for k2, v2 in d2.items():
-        if k2 not in ignored and k2 not in d1:
-            return False
-    return True
-
-
 def get_coord(string):
     lines = string.split('\n')
     line = len(lines)
